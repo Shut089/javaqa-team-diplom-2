6385cbcc2b1c96e7948b46caae764b7320febfb5
Alexey Utkin <cedrickobayashi@gmail.com>
2025-11-11 23:05:48 +0300

    Issues in credit fixed

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -17,7 +17,17 @@ public class CreditAccount extends Account {
      * @param rate - неотрицательное число, ставка кредитования для расчёта долга за отрицательный баланс
      */
     public CreditAccount(int initialBalance, int creditLimit, int rate) {
-        if (rate <= 0) {
+        if (initialBalance < 0) {
+            throw new IllegalArgumentException(
+                    "Начальный баланс для счёта не может быть отрицательным, а у вас: " + initialBalance
+            );
+        }
+        if (creditLimit < 0) {
+            throw new IllegalArgumentException(
+                    "Кредитный лимит не может быть отрицательным, а у вас: " + creditLimit
+            );
+        }
+        if (rate < 0) {
             throw new IllegalArgumentException(
                     "Накопительная ставка не может быть отрицательной, а у вас: " + rate
             );
@@ -42,9 +52,8 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = balance - amount;
-        if (balance > -creditLimit) {
-            balance = -amount;
+        if ((balance - amount) >= -creditLimit) {
+            balance = balance - amount;
             return true;
         } else {
             return false;
@@ -67,7 +76,7 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
        }
-        balance = amount;
+        balance = balance + amount;
         return true;
     }
 
